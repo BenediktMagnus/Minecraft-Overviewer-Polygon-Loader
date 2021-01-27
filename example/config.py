765c5cd79e440a52814f74031475624211fc574d
Benedikt Magnus <magnus@magnuscraft.de>
2021-01-27 15:16:30 +0100

    Added example files

---
+++ example/config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.path.append('../dist')
+
+from PolygonLoader import PolygonLoader
+
+worlds['My World'] = '/home/username/server/world'
+
+polygonLoader = PolygonLoader('Properties')
+polygonLoader.load('properties.json')
+
+renders['normalrender'] = {
+    'world': 'My World',
+    'title': 'Normal Render of My World',
+    'manualpois': polygonLoader.getPois(),
+    'markers': [
+        polygonLoader.getMarker()
+    ],
+}
+
+outputdir = '/home/username/mcmap'
